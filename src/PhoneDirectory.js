f1a5339ee01ab05f3e0e69f93346f549a7172a3c
SonaliBhagwatkar <sonali.bhagwatkar@gmail.com>
2019-07-27 00:41:33 +0530

    Added routing for Add Subscriber page and Show Subscribers page

--- src/PhoneDirectory.js
+++ src/PhoneDirectory.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import AddSubscriber from './AddSubscriber';
 import ShowSubscribers from './ShowSubscribers';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 class PhoneDirectory extends Component{
 
@@ -29,15 +30,17 @@ class PhoneDirectory extends Component{
             newSubscriber.id = 1;
            }
         subscribersList.push(newSubscriber);
-        this.setState({subscribersList: subscribersList});
-        console.log("Phone Directory")
-        console.log(this.state.subscribersList);
-    }
+        this.setState({subscribersList: subscribersList})
+       }
 
     render(){
         return(
-            //<AddSubscriber addSubscriberHandler={this.addSubscriberHandler} />
-            <ShowSubscribers subscribersList={this.state.subscribersList}/>
+           <Router>
+               <div className="main-container">
+               <Route exact path='/' render={(props) => <ShowSubscribers {...props} subscribersList={this.state.subscribersList} />} />
+               <Route exact path='/add' render={({history}, props) => <AddSubscriber history={history} {...props} addSubscriberHandler={this.addSubscriberHandler} />} />
+               </div>
+           </Router>
         )
     }
 }
--- src/ShowSubscribers.js
+++ src/ShowSubscribers.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import Header from "./Header";
+import Header from "./common/Header";
 import './ShowSubscribers.css';
+import './common/common.css';
+import {Link} from 'react-router-dom';
+
 
 // let subscribers = [
     //   {
@@ -24,7 +27,7 @@ class ShowSubscribers extends Component {
      <div className="component-container">
           <Header heading="Phone Directory"/>
           <div className="component-body-container">
-          <button className="custom-btn add-btn"> Add</button>
+         <Link to="/add"><button className="custom-btn add-btn"> Add</button></Link>
 
           <div className="grid-container heading-container">
               <span className="grid-item name-heading">Name</span>
